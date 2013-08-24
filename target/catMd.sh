9e2aab60673c6f1bf3b99758e0a6eab0ed3c9f77
Kuba Brecka <kuba.brecka@gmail.com>
2013-08-24 18:06:27 +0200

    initial commit

---
+++ target/catMd.sh
@@ -0,0 +1,144 @@
+
+
+	
+	
+	
+
+	
+		
+		echo "# Introduction to EverBEEN"
+		
+			
+			
+
+			
+				
+		cat src/book/markdown/foreword.md
+		echo
+	
+
+				
+		cat src/book/markdown/study.md
+		echo
+	
+
+				
+		cat src/book/markdown/audience.md
+		echo
+	
+
+				
+		cat src/book/markdown/history.md
+		echo
+	
+
+				
+		cat src/book/markdown/pgoals.md
+		echo
+	
+			
+		
+
+		
+		echo "# EverBEEN user guide"
+		
+			
+			
+
+			
+				
+		cat src/book/markdown/requirements.md
+		echo
+	
+
+				
+		cat src/book/markdown/deployment.md
+		echo
+	
+
+				
+		cat src/book/markdown/controls.md
+		echo
+	
+
+				
+		cat src/book/markdown/taskapi.md
+		echo
+	
+
+				
+		cat src/book/markdown/swrepo.md
+		echo
+	
+
+				
+		cat src/book/markdown/persistence.md
+		echo
+	
+
+				
+		cat src/book/markdown/configuration.md
+		echo
+	
+
+				
+		cat src/book/markdown/recommendations.md
+		echo
+	
+			
+		
+
+		
+		echo "# EverBEEN developer documentation"
+		
+			
+			
+
+			
+				
+		cat src/book/markdown/dgoals.md
+		echo
+	
+
+				
+		cat src/book/markdown/timeline.md
+		echo
+	
+
+				
+		cat src/book/markdown/architecture.md
+		echo
+	
+
+				
+		cat src/book/markdown/services.md
+		echo
+	
+
+				
+		cat src/book/markdown/modular.md
+		echo
+	
+
+				
+		cat src/book/markdown/technologies.md
+		echo
+	
+
+				
+		cat src/book/markdown/features.md
+		echo
+	
+
+				
+		cat src/book/markdown/limitations.md
+		echo
+	
+
+				
+		cat src/book/markdown/javadoc.md
+		echo
+	
+			
+		
+	
